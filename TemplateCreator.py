3fa0e58c5726bf0f9b051c562755f18a01955cd5
Silvio <silviosanto6605@gmail.com>
2019-07-29 11:57:05 +0200

    First Commit

---
+++ TemplateCreator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+try:
+    import sys
+    import os
+
+    ProjectConfirmed = False
+
+    while ProjectConfirmed == False:
+        NewProject = input("Please type 'create ' to create a default template for HTML/CSS/JS project, type delete to delete it...   ")
+
+        if NewProject == "create":
+            if not os.path.exists("Project1"):
+                os.system("mkdir Project1 && cd Project1 && touch index.html && mkdir css img js ")
+                ProjectConfirmed = True
+                print("Template created. Exiting...")
+                sys.exit()
+            else:
+                deleteconf= input("You have another folder with this name. If you type yes, you are going to detroy the actual folder. To abort press ctrl+c Continue? ")
+                if deleteconf == "yes":
+                    os.system("rm -rf Project1")
+                    sys.exit(0)
+
+
+        if NewProject == "delete":
+            ProjectConfirmed == True
+            os.system("rm -rf Project1")
+            print("Project deleted!")
+            sys.exit(0)
+
+
+
+        else:
+            NewProject = input("Please, retype your answer. Press enter then retype ")
+
+except KeyboardInterrupt:
+    print("Have a good day... See you soon :) ")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='TemplateCreator',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Silvio Santoriello',
+    author_email='dev.silviosanto6605@gmail.com',
+    description='A template creator for several type of projects'
+)
